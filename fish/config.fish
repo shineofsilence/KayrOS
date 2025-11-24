288fd29ec422d35be226556268cb19fe39046be0
Kayros <order.exterminatus@yandex.ru>
2025-11-24 16:57:50 +0300

    Настроил окно иконок и начал настройку fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,22 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-# === Функция y (Вход в Yazi с запоминанием папки) ===
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
 end
 
-# === Автозапуск Yazi только в интерактивном режиме ===
-if status is-interactive
-    if test "$TERM" = "xterm-kitty"; and test -z "$YAZI_LEVEL"
+# === YAZI CONFIGURATION (Только для хоста) ===
+# Проверяем: (1) Мы НЕ в Distrobox, (2) Yazi установлен в системе
+if test -z "$DISTROBOX_ENTER"; and command -q yazi
+    # Функция-обертка для выхода в текущую папку
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+    # Автозапуск только в Kitty и интерактивном режиме
+    if status is-interactive; and test "$TERM" = "xterm-kitty"; and test -z "$YAZI_LEVEL"
         y
     end
 end
+starship init fish | source
