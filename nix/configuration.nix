c4249187ab74c85fcc02ac03b0dfd177cccd9e73
Kayros <order.exterminatus@yandex.ru>
2025-11-28 01:14:10 +0300

    Переход на стабильный релиз 25.11

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -7,7 +7,7 @@
     ];
 
   # =============== Версия системы ==================
-  system.stateVersion = "25.05"; 
+  system.stateVersion = "25.11"; 
 
   # ================= Загрузчик =====================
   boot.loader.systemd-boot.enable = true;
